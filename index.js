e358908a6380fa428688a146aec6103688d44b19
Colin2030 <129393734+Colin2030@users.noreply.github.com>
2025-12-15 17:43:36 +0000

    updates

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import TelegramBot from 'node-telegram-bot-api';
 const app = express();
 app.use(express.json());
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new TelegramBot(process.env.8540305777:AAEesA51C2ItFEIu4fNM7Rmv48Gub6PRNUY);
 
-app.post(`/webhook/${process.env.TELEGRAM_BOT_TOKEN}`, (req, res) => {
+app.post(`/webhook/${process.env.8540305777:AAEesA51C2ItFEIu4fNM7Rmv48Gub6PRNUY}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
